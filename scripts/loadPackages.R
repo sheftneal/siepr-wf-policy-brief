de59808f0901e3014dc521cb8688caa696b7079c
sheftneal <sheftneal@stanford.edu>
2020-10-14 14:46:09 -0700

    remove packages that are not used in these scripts

--- scripts/loadPackages.R
+++ scripts/loadPackages.R
@@ -1,22 +1,6 @@
 #ported over from another project. Haven't double checked that these packages are all utilized here.
 library(tidyverse)
 library(data.table)
-library(sf)
 library(sp)
-library(rgeos)
+library(wesanderson)
 library(rgdal)
-library(geosphere)
-library(ncdf4)
-library(dplyr)
-library(tidyr)
-library(stringr)
-library(splines)
-library(readr)
-library(data.table)
-library(raster)
-library(gdata) #to read xls
-library(openxlsx) # to read xlsx
-library(signal) #for interpolation
-library(imputeTS) #for na_kalman
-library(zoo)
-library(tidycensus)
